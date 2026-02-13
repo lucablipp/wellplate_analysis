079aacceaa4a1dee572d976da1408aaa766971e9
Luca Lippert <luca.b.lippert7@gmail.com>
2026-02-13 04:14:00 -0800

    Update __init__.py

--- wellplate_analysis/__init__.py
+++ wellplate_analysis/__init__.py
@@ -2,5 +2,6 @@ from .io import load_xlsx
 from .processing import handle_ovrflw, index_to_time, normalize_by_OD, rename_sample_columns, average_replicates, rename_and_average 
 from .calibration import rfu_to_mefl, plot_calibration
 from .plotting import plot_columns
+from .run_pipeline import run_pipeline
 
 __version__ = "0.1.0"
