5ed3eb5bd8a986dc0488980a06d44d5f393aa8f1
Lucas Lippert <otoudai@Lucas-MacBook-Air-2.local>
2026-02-13 16:58:38 -0800

    Added CLI

---
+++ cli.py
@@ -0,0 +1,20 @@
+import argparse
+from .run_pipeline import run_pipeline
+
+def main():
+    parser = argparse.ArgumentParser(
+        description = "Run the well plate analysis pipeline from a YAML config file"
+    )
+
+    parser.add_argument(
+        "config",
+        type=str,
+        help="Path to YAML configuration file"
+    )
+
+    args = parse.parse_args()
+
+    run_pipeline(args.config)
+
+    if __name__ == "__main__":
+        main()
